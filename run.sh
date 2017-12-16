18fd40f5e52b00375bc72255c3adcc986980647e
Arran France <arranf@gmail.com>
2017-12-16 17:15:05 +0000

    Ensure heroes-talents is only cloned if need be

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-git clone git@github.com:heroespatchnotes/heroes-talents.git
+if [ ! -d "heroes-talents" ]; then
+    git clone git@github.com:heroespatchnotes/heroes-talents.git
+fi
 python3 convert_heroes.py
