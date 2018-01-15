70257ccb17a4062cdff0a3ddedee2c8d535ac29c
Arran France <arranf@gmail.com>
2018-01-15 15:09:52 +0000

    Update SHA

--- publish.sh
+++ publish.sh
@@ -4,4 +4,9 @@
 # export AWS_ACCESS_KEY_ID=YOURACCESSKEYHERE
 # export AWS_SECRET_ACCESS_KEY=YOURSECRETACCESKEYHERE
 # export AWS_DEFAULT_REGION=eu-west-1
+
+# Create sha.txt of repo
+touch upload/sha.txt
+cd heroes-talents && git rev-parse --verify HEAD > ../upload/sha.txt
+
 aws s3 sync upload/ s3://data.heroescompanion.com
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 if [ ! -d "heroes-talents" ]; then
     git clone git@github.com:heroespatchnotes/heroes-talents.git
 fi
+rm -rf upload
 mkdir upload
 python3 convert_heroes.py
 python3 create_json.py
