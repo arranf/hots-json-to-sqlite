0873f05901c4c5ceaef1f8faa03417c6e43fedfa
Arran France <arranf@gmail.com>
2018-01-30 00:16:45 +0000

    Add patch number to json

--- create_json.py
+++ create_json.py
@@ -1,7 +1,8 @@
-# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+import re
 import json
 import sqlite3
-import os
+import subprocess
 import datetime
 
 def dict_factory(cursor, row):
@@ -9,6 +10,25 @@ def dict_factory(cursor, row):
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
+
+def get_sha(repo):
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=repo).decode('ascii').strip()
+    return sha
+
+def get_patch_number():
+    import http.client
+    conn = http.client.HTTPSConnection("api.github.com")
+    conn.request("GET", "/repos/heroespatchnotes/heroes-talents/commits", headers={'User-Agent': 'hero_data_to_json'})
+    res = conn.getresponse()
+    data = res.read()
+    response_data = json.loads(data.decode("utf-8"))
+    patch_number = ''
+    for commit in response_data:
+        match = re.search(r'\d+\.\d+', commit['commit']['message'])
+        if match is not None:
+            patch_number = match.group()
+            break
+        return patch_number
 
 DB_FILE_NAME = "upload/heroes_companion.db"
 HERO_TABLE_NAME = "heroes"
@@ -18,22 +38,24 @@ CONNECTION = sqlite3.connect(DB_FILE_NAME, detect_types=sqlite3.PARSE_DECLTYPES|
 CURSOR = CONNECTION.cursor()
 
 with CONNECTION:
-        CURSOR.row_factory = dict_factory
-            
-        CURSOR.execute("SELECT * FROM " + HERO_TABLE_NAME)
-        hero_results = CURSOR.fetchall()
-        CURSOR.execute("SELECT * FROM " + TALENTS_TABLE_NAME)
-        talent_results = CURSOR.fetchall()
-        CURSOR.execute("SELECT * FROM " + ABILITIES_TABLE_NAME)
-        abilities_results = CURSOR.fetchall()
+    CURSOR.row_factory = dict_factory
+        
+    CURSOR.execute("SELECT * FROM " + HERO_TABLE_NAME)
+    hero_results = CURSOR.fetchall()
+    CURSOR.execute("SELECT * FROM " + TALENTS_TABLE_NAME)
+    talent_results = CURSOR.fetchall()
+    CURSOR.execute("SELECT * FROM " + ABILITIES_TABLE_NAME)
+    abilities_results = CURSOR.fetchall()
+    
+    results = {
+        "id": datetime.datetime.utcnow().isoformat(),
+        "patch": get_patch_number(),
+        "sha": get_sha('./heroes-talents'),
+        "heroes" : hero_results,
+        "talents": talent_results,
+        "abilities": abilities_results
+    }
 
-        results = {
-            "id": datetime.datetime.utcnow().isoformat(),
-            "heroes" : hero_results,
-            "talents": talent_results,
-            "abilities": abilities_results
-        }
-
-        with open('./upload/data.json', 'a') as f:
-            json.dump(results, f, ensure_ascii=False)
+    with open('./upload/data.json', 'w') as f:
+        json.dump(results, f, ensure_ascii=False)
 CONNECTION.close()
